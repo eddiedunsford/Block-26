5244381dcd71b688f256a4896a2c2eec7dbc11fa
eddie dunsford <eddie.dunsford@gmail.com>
2024-11-01 16:08:33 -0400

    add files

---
+++ Contact-list/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+const [contacts, setContacts] = useState(dummyContacts)
+console.log("Contacts: ", contacts)
+function App() {
+  return (
+    <div>
+      export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+  }
+    </div>
+  );
+}
+
+export default App;
